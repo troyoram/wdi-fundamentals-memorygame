db22dffacc605c3895be204f1cca7c96bbf9493b
troyoram <oram0720@gmail.com>
2018-09-16 18:21:33 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,27 @@
-console.log("Up and running!");
+// console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = ['queen', 'queen', 'king', 'king']
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+const cardsInPlay = []
+
+const cardOne = cards[0]
+
+cardsInPlay.push(cardOne)
+
+console.log('User flipped ' + cardOne)
+
+const cardTwo = cards[2]
+
+cardsInPlay.push(cardTwo)
+
+console.log('User flipped ' + cardTwo)
+
+if (cardsInPlay.length == 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!')
+	} else {
+		alert('Sorry, try again.')
+	}
+}
+
+// console.log(cardsInPlay)

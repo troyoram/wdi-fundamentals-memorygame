c50953a20e347e1fc37af31827c7b685a19076d4
troyoram <oram0720@gmail.com>
2018-09-16 21:09:48 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,31 @@
 // console.log("Up and running!");
 
-const cards = ['queen', 'queen', 'king', 'king']
+// const cards = ['queen', 'queen', 'king', 'king']
+const cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: "images/queen-of-diamonds.png"
+
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png"
+
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png"
+
+}
+]
 
 const cardsInPlay = []
 
@@ -13,8 +38,10 @@ const checkForMatch = function () {
 }
 
 const flipCard = function (cardId) {
-	cardsInPlay.push(cards[cardId])
-	console.log('User flipped ' + cards[cardId])
+	cardsInPlay.push(cards[cardId].rank)
+	console.log('User flipped ' + cards[cardId].rank)
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
 	if (cardsInPlay.length == 2) {
 		checkForMatch()
 	}
